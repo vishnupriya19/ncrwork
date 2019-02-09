94728e5f42b84b03d2a8d3f1aeea7d7bedbe53ae
vishnupriya19 <svpjemunigani@gmail.com>
2019-02-09 07:27:02 +0530

    Added a prg in Assignment4

--- C_Programs/Assignment4/A4_pg12_Del.cpp
+++ C_Programs/Assignment4/A4_pg12_Del.cpp
@@ -26,9 +26,9 @@ int main()
 {
 	char s1[50], s2[50];
 	printf("enter string1\n");
-	scanf("%s", s1);
+	scanf("%[^\n]s", s1);
 	printf("enter string2\n");
-	scanf("%s", s2);
+	scanf(" %[^\n]s", s2);
 	deletes2(s1, s2);
 	printf("%s", s1);
 	return 0;
