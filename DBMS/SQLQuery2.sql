1917425a25b629dc4b0033968a5bb2ead41d3ff3
vishnupriya19 <svpjemunigani@gmail.com>
2019-03-15 11:18:41 +0530

    Added DBSM

--- DBMS/SQLQuery2.sql
+++ DBMS/SQLQuery2.sql
@@ -1,5 +1,111 @@
 use practice;
 
+CREATE TABLE dept(
+  deptno int,
+  dname  nVARCHAR(14),
+  loc    nVarchar(13),
+  CONSTRAINT pk_dept PRIMARY KEY (deptno));
+
+CREATE TABLE emp(
+  empno    int,
+  ename    nVARCHAR(10),
+  job      nVARCHAR(9),
+  mgr      INT,
+  hiredate DATE,
+  sal      DECIMAL(7,2),
+  comm     DECIMAL(7,2),
+  deptno   INT,
+  CONSTRAINT pk_emp PRIMARY KEY (empno),
+  constraint fk_deptno foreign key (deptno) references dept(deptno)
+);
+
+CREATE TABLE salgrade(
+  grade int,
+  losal int,
+  hisal int
+);
+
+INSERT INTO emp VALUES(
+ 7839, 'KING', 'PRESIDENT', null,
+ convert(datetime,'1981-11-17'),
+ 5000, null, 10);
+
+INSERT INTO emp VALUES(
+ 7698, 'BLAKE', 'MANAGER', 7839,
+ ('1-5-1981'),
+ 2850, null, 30);
+
+INSERT INTO emp VALUES(
+ 7782, 'CLARK', 'MANAGER', 7839,
+ '9-6-1981',
+ 2450, null, 10);
+
+INSERT INTO emp VALUES(
+ 7566, 'JONES', 'MANAGER', 7839,
+ ('2-4-1981'),
+ 2975, null, 20);
+
+INSERT INTO emp VALUES(
+ 7788, 'SCOTT', 'ANALYST', 7566,
+('13-JUL-87'),
+ 3000, null, 20);
+
+INSERT INTO emp VALUES(
+ 7902, 'FORD', 'ANALYST', 7566,
+('3-12-1981'),
+ 3000, null, 20 );
+
+INSERT INTO emp VALUES(
+ 7369, 'SMITH', 'CLERK', 7902,
+ convert(datetime,'1980-12-17'),
+ 800, null, 20 );
+
+INSERT INTO emp VALUES(
+ 7499, 'ALLEN', 'SALESMAN', 7698,
+ ('1981-2-20'),
+ 1600, 300, 30);
+
+INSERT INTO emp VALUES(
+ 7521, 'WARD', 'SALESMAN', 7698,
+ ('1981-2-22'),
+ 1250, 500, 30 );
+
+INSERT INTO emp VALUES(
+ 7654, 'MARTIN', 'SALESMAN', 7698,
+ ('1981-9-28'),
+ 1250, 1400, 30 );
+
+INSERT INTO emp VALUES(
+ 7844, 'TURNER', 'SALESMAN', 7698,
+ ('1981-8-9'),
+ 1500, 0, 30);
+
+INSERT INTO emp VALUES(
+ 7876, 'ADAMS', 'CLERK', 7788,
+ ('13-JUL-87'),
+ 1100, null, 20 );
+
+INSERT INTO emp VALUES(
+ 7900, 'JAMES', 'CLERK', 7698,
+ ('3-12-1981'),
+ 950, null, 30 );
+
+INSERT INTO emp VALUES(
+ 7934, 'MILLER', 'CLERK', 7782,
+ ('1982-1-23'),
+ 1300, null, 10 );
+
+INSERT INTO dept VALUES(10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO dept VALUES(20, 'RESEARCH', 'DALLAS');
+INSERT INTO dept VALUES(30, 'SALES', 'CHICAGO');
+INSERT INTO dept VALUES(40, 'OPERATIONS', 'BOSTON');
+
+INSERT INTO salgrade VALUES (1, 700, 1200);
+INSERT INTO salgrade VALUES (2, 1201, 1400);
+INSERT INTO salgrade VALUES (3, 1401, 2000);
+INSERT INTO salgrade VALUES (4, 2001, 3000);
+INSERT INTO salgrade VALUES (5, 3001, 9999);
+
 1. select * from emp where sal between 1000 and 2000;
 
 2. select deptno,dname from dept order by dname;
